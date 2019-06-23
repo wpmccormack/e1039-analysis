cb0582ecd9cba7ef926e88fa13ecfdf813551aa8
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2019-06-23 15:51:09 -0500

    The contents of AnaRealDst became more meaningful (thus useful).

--- AnaRealDst/macro/Fun4RealDst.C
+++ AnaRealDst/macro/Fun4RealDst.C
@@ -7,14 +7,11 @@ int Fun4RealDst(const int run=46, const int nevent=0)
 {
   gSystem->Load("libana_real_dst.so");
 
-  const char* dir_in  = "/data2/e1039/dst";
-  ostringstream oss;
-  oss << setfill('0') << dir_in << "/run_" << setw(6) << run << "_spin.root";
-  string fn_in = oss.str();
-
   Fun4AllServer* se = Fun4AllServer::instance();
   //se->Verbosity(1);
 
+  string fn_in = UtilOnline::GetDstFileDir() + "/" + UtilOnline::RunNum2DstFile(run);
+  cout << "DST file = " << fn_in << endl;
   Fun4AllInputManager *in = new Fun4AllDstInputManager("RealDst");
   in->fileopen(fn_in);
   se->registerInputManager(in);
--- AnaRealDst/src/AnaRealDst.h
+++ AnaRealDst/src/AnaRealDst.h
@@ -2,25 +2,26 @@
 #define _ANA_REAL_DST__H_
 #include <fun4all/SubsysReco.h>
 class TFile;
-class TNtuple;
+class TTree;
 class TH1;
 class TH2;
 
+/// An example class to analyze the E1039 DST file.
 class AnaRealDst: public SubsysReco {
+  static const std::vector<std::string> list_det_name;
+  std::vector<int> list_det_id;
+
   TFile* f_out;
-  TNtuple* nt;
-  TH1* h1_nhit_h1x;
-  TH1* h1_nhit_h1y;
-  TH1* h1_nhit_h2x;
-  TH1* h1_nhit_h2y;
-  TH1* h1_nhit_h3x;
-  TH1* h1_nhit_h4x;
-  TH1* h1_nhit_h4y1;
-  TH1* h1_nhit_h4y2;
+  TTree* tree;
+  char   b_det_name[16];
+  int    b_det;
+  int    b_ele;
+  double b_time;
+  TH1*   h1_ele[99];
 
  public:
   AnaRealDst() {;}
-  virtual ~AnaRealDst();
+  virtual ~AnaRealDst() {;}
   int Init(PHCompositeNode *topNode);
   int InitRun(PHCompositeNode *topNode);
   int process_event(PHCompositeNode *topNode);
